79ab401c589f3c2b767dab8e63ebd935cf09a9bd
Sachin Math <sachinmath619@gmail.com>
2024-09-09 14:04:28 +0530

    intial commit

---
+++ src/components/Users.js
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react";
+import useAxiosPrivate from "../hooks/useAxiosPrivate";
+import { useNavigate, useLocation } from "react-router-dom";
+
+const Users = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const axiosPrivate = useAxiosPrivate();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    let isMounted = true;
+    const controller = new AbortController();
+
+    const getUsers = async () => {
+      try {
+        const response = await axiosPrivate.get("/users", {
+          signal: controller.signal,
+        });
+        if (isMounted) {
+          setUsers(response.data);
+          setLoading(false);
+        }
+      } catch (err) {
+        console.error(err);
+        navigate("/login", { state: { from: location }, replace: true });
+      }
+    };
+
+    getUsers();
+
+    return () => {
+      isMounted = false;
+      controller.abort();
+    };
+  }, [axiosPrivate, navigate, location]);
+
+  return (
+    <article>
+      <h2>Users List</h2>
+      {loading ? (
+        <p>Loading...</p>
+      ) : users.length ? (
+        <ul>
+          {users.map((user, i) => (
+            <li key={i}>{user?.username}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No users to display</p>
+      )}
+    </article>
+  );
+};
+
+export default Users;
